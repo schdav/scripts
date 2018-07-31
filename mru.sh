2af5bdce5d4b04ad89d12c4803ea8dc83123bdbb
schdav <schdav@users.noreply.github.com>
2018-07-31 19:47:10 +0200

    Reformat code

--- mru.sh
+++ mru.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 declare -a arr=("com.apple.LSSharedFileList.RecentApplications.sfl2"
-								"com.apple.LSSharedFileList.RecentDocuments.sfl2"
-								"com.apple.LSSharedFileList.RecentServers.sfl2"
-								"com.apple.LSSharedFileList.RecentHosts.sfl2")
+                "com.apple.LSSharedFileList.RecentDocuments.sfl2"
+                "com.apple.LSSharedFileList.RecentServers.sfl2"
+                "com.apple.LSSharedFileList.RecentHosts.sfl2")
 
 cd ~/Library/Application\ Support/com.apple.sharedfilelist
 
 for i in "${arr[@]}"; do
-	if [ -f "$i" ]; then
-		mv "$i" "$i".bak
-		touch "$i"
-	fi
+  if [ -f "$i" ]; then
+    mv "$i" "$i".bak
+    touch "$i"
+  fi
 done
 
 cd com.apple.LSSharedFileList.ApplicationRecentDocuments
 
 for j in ./*.sfl2; do
-	mv  "$j" "$j".bak
-	touch "$j"
+  mv  "$j" "$j".bak
+  touch "$j"
 done
